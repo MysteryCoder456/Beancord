2978dc088672c0d40761e9fa894dc26b6e057249
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-03-13 10:06:54 +0400

    added boilerplate Xcode project

---
+++ iOSChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSChatApp
+//
+//  Created by Rehatbir Singh on 03/13/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSChatApp/iOSChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChatAppApp.swift
+//  iOSChatApp
+//
+//  Created by Rehatbir Singh on 03/13/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
