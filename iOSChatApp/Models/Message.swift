4166ddeee62acefb7b30b307921e1dd85fc2e9d2
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-03-16 12:05:03 +0400

    preview message list

--- iOSChatApp/Models/Message.swift
+++ iOSChatApp/Models/Message.swift
@@ -17,3 +17,10 @@ struct Message: Identifiable, Codable {
     var content: String
     var dateSent = Date()
 }
+
+var previewMessageList = [
+    Message(id: UUID().uuidString, authorID: "uWnt9eEpTWNOnXUxqxnx5DiSK9K2", guildID: previewGuild.id!, content: "Hello"),
+    Message(id: UUID().uuidString, authorID: "uWnt9eEpTWNOnXUxqxnx5DiSK9K2", guildID: previewGuild.id!, content: "How do you do?"),
+    Message(id: UUID().uuidString, authorID: "uWnt9eEpTWNOnXUxqxnx5DiSK9K2", guildID: previewGuild.id!, content: "I'm great"),
+    Message(id: UUID().uuidString, authorID: "uWnt9eEpTWNOnXUxqxnx5DiSK9K2", guildID: previewGuild.id!, content: "This is a test...")
+]
